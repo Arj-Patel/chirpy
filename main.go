6482be030a632531a886f0060a311a9339412601
Arj-Patel <arjpatel03@gmail.com>
2024-06-13 17:42:49 +0530

    serving static files from root directory

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	// Serve static files from the root directory
+	fs := http.FileServer(http.Dir("."))
+	mux.Handle("/", fs)
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
